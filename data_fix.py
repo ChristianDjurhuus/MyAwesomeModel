27135e18353f77e2fdc2210b67194263a80892f1
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2022-01-05 10:28:48 +0100

    Fixing error in dvc

--- data_fix.py
+++ data_fix.py
@@ -5,7 +5,7 @@ import numpy as np
 path = '/Users/christiandjurhuus/Documents/DTU/6_semester/ml_ops/dtu_mlops/data/corruptmnist/'
 os.chdir(path)
 
-file_list = ["train_0.npz", "train_1.npz", "train_2.npz", "train_3.npz", "train_4.npz"]
+file_list = ["train_0.npz", "train_1.npz", "train_2.npz", "train_3.npz", "train_4.npz", "train_5.npz", "train_6.npz", "train_7.npz"]
 
 data_all = [np.load(fname) for fname in file_list]
 merged_data = {}
